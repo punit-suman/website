70b7a1773d26f2144dba3a0b68abe65e7c94bc18
punit-fm <punit.suman@flairminds.com>
2024-10-12 20:48:07 +0530

    lint errors fix

--- utils/resumeText.js
+++ utils/resumeText.js
@@ -1,204 +1,204 @@
 export const headers = {
-    aboutMe: 'ABOUT ME',
-    education: 'EDUCATIONAL QUALIFICATIONS',
-    scholastic: 'SCHOLASTIC ACHIEVEMENTS',
-    technical: 'TECHNICAL SKILLS',
-    softSkills: 'SOFT SKILLS',
-    technicalSub: {
-        progLang: 'Programming Languages',
-        webTech: 'Web Technologies & Frameworks',
+	aboutMe: 'ABOUT ME',
+	education: 'EDUCATIONAL QUALIFICATIONS',
+	scholastic: 'SCHOLASTIC ACHIEVEMENTS',
+	technical: 'TECHNICAL SKILLS',
+	softSkills: 'SOFT SKILLS',
+	technicalSub: {
+		progLang: 'Programming Languages',
+		webTech: 'Web Technologies & Frameworks',
 		dbms: 'Database Management Systems',
 		cloud: 'Cloud Services',
 		otherTools: 'Other Tools'
-    },
-    connect: 'CONNECT'
-}
+	},
+	connect: 'CONNECT'
+};
 
 export const progLangs = [
-    {
-        name: 'Javascript',
-        src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
+	{
+		name: 'Javascript',
+		src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
 		percent: 80
-    },
-    {
-        name: 'SQL',
-        src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
+	},
+	{
+		name: 'SQL',
+		src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
 		percent: 70
-    },
-    {
-        name: 'C#',
-        src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw'
-    },
-    {
-        name: 'Python',
-        src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw'
-    },
-    {
-        name: 'Go',
-        src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
+	},
+	{
+		name: 'C#',
+		src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw'
+	},
+	{
+		name: 'Python',
+		src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw'
+	},
+	{
+		name: 'Go',
+		src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
 		percent: 30
-    },
-    {
-        name: 'C',
-        src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
+	},
+	{
+		name: 'C',
+		src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
 		percent: 25
-    },
-    {
-        name: 'C++',
-        src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
+	},
+	{
+		name: 'C++',
+		src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
 		percent: 25
-    }
-]
+	}
+];
 
 export const webTechs = [
-    {
-        name: 'NodeJS',
-        src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
+	{
+		name: 'NodeJS',
+		src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
 		percent: 80
-    },
-    {
-        name: 'ExpressJS',
-        src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
+	},
+	{
+		name: 'ExpressJS',
+		src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
 		percent: 80
-    },
-    {
-        name: 'ReactJS',
-        src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
+	},
+	{
+		name: 'ReactJS',
+		src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
 		percent: 80
-    },
-    {
-        name: 'HTML',
-        src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
+	},
+	{
+		name: 'HTML',
+		src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
 		percent: 80
-    },
-    {
-        name: 'CSS',
-        src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
+	},
+	{
+		name: 'CSS',
+		src: 'https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw',
 		percent: 80
-    },
-    {
-        name: '.NET',
-        src: '',
-		percent: 60
-    },
+	},
 	{
-        name: 'Strapi',
-        src: '',
+		name: '.NET',
+		src: '',
 		percent: 60
-    },
+	},
 	{
-        name: 'Angular',
-        src: '',
+		name: 'Strapi',
+		src: '',
 		percent: 60
-    },
+	},
 	{
-        name: 'GraphQL',
-        src: '',
+		name: 'Angular',
+		src: '',
 		percent: 60
-    },
+	},
 	{
-        name: 'Flask',
-        src: '',
+		name: 'GraphQL',
+		src: '',
 		percent: 60
-    },
+	},
 	{
-        name: 'Django',
-        src: '',
+		name: 'Flask',
+		src: '',
 		percent: 60
-    },
+	},
 	{
-        name: 'Sequelize',
-        src: '',
+		name: 'Django',
+		src: '',
 		percent: 60
-    }
-]
+	},
+	{
+		name: 'Sequelize',
+		src: '',
+		percent: 60
+	}
+];
 
 export const dbms = [
-    {
-        name: 'MySQL',
-        src: '',
+	{
+		name: 'MySQL',
+		src: '',
 		percent: 80
-    },
-    {
-        name: 'PostgreSQL',
-        src: '',
+	},
+	{
+		name: 'PostgreSQL',
+		src: '',
 		percent: 80
-    },
-    {
-        name: 'MS-SQL Server',
-        src: '',
+	},
+	{
+		name: 'MS-SQL Server',
+		src: '',
 		percent: 80
-    },
-    {
-        name: 'MongoDB',
-        src: '',
+	},
+	{
+		name: 'MongoDB',
+		src: '',
 		percent: 80
-    }
-]
+	}
+];
 
 export const cloud = [
-    {
-        name: 'AWS',
-        src: '',
-		percent: 80
-    },
-    {
-        name: 'Netlify',
-        src: '',
-		percent: 80
-    },
-    {
-        name: 'Heroku',
-        src: '',
-		percent: 80
-    },
-    {
-        name: 'Cyclic.sh',
-        src: '',
-		percent: 80
-    },
 	{
-        name: 'Azure',
-        src: '',
+		name: 'AWS',
+		src: '',
 		percent: 80
-    }
-]
+	},
+	{
+		name: 'Netlify',
+		src: '',
+		percent: 80
+	},
+	{
+		name: 'Heroku',
+		src: '',
+		percent: 80
+	},
+	{
+		name: 'Cyclic.sh',
+		src: '',
+		percent: 80
+	},
+	{
+		name: 'Azure',
+		src: '',
+		percent: 80
+	}
+];
 
 export const otherTools = [
-    {
-        name: 'VS Code',
-        src: ''
-    },
-    {
-        name: 'Visual Studio',
-        src: ''
-    },
-    {
-        name: 'Github',
-        src: ''
-    },
-    {
-        name: 'Postman',
-        src: ''
-    },
 	{
-        name: 'Trello',
-        src: ''
-    },
+		name: 'VS Code',
+		src: ''
+	},
 	{
-        name: 'ESLint',
-        src: ''
-    },
+		name: 'Visual Studio',
+		src: ''
+	},
 	{
-        name: 'Google Data Studio',
-        src: ''
-    },
+		name: 'Github',
+		src: ''
+	},
 	{
-        name: 'draw.io',
-        src: ''
-    },
+		name: 'Postman',
+		src: ''
+	},
 	{
-        name: 'figma',
-        src: ''
-    }
-]
+		name: 'Trello',
+		src: ''
+	},
+	{
+		name: 'ESLint',
+		src: ''
+	},
+	{
+		name: 'Google Data Studio',
+		src: ''
+	},
+	{
+		name: 'draw.io',
+		src: ''
+	},
+	{
+		name: 'figma',
+		src: ''
+	}
+];
